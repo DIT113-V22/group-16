5a5420146d4a1c6f43fe8071a369a021883932dc
Qianyuan_Wang <gusqianwa@student.gu.se>
2022-05-17 14:03:57 +0200

    backend and frontend code

--- AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.androidapp;
 
+
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
@@ -18,6 +21,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -28,52 +32,63 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private MqttAndroidClient mMqttClient;
-    private static final String MQTT_SERVER = "tcp://192.168.0.242:1883";
     private static final String TAG = "SmartCarMqttController";
+    private static final String LOCALHOST = "192.168.0.149";
+    private static final String MQTT_SERVER = "tcp://" + LOCALHOST + ":1883";
+
     static String USERNAME = "admin";
-    static String PASSWORD = "hivemq";
-    private String topic = "/Group/16";
+    static String PASSWORD = "";  // **
+
     private String controlTopic = "/Group/16/Control";
-    private String cruiseTopic = "/Group/16/Cruise";
     private String cameraTopic = "/Group/16/Camera";
 
-    private String leftDistanceTopic = "/Group/16/Distance/Left";
-    private String rightDistanceTopic = "/Group/16/Distance/Right";
-    private String frontDistanceTopic = "/Group/16/Distance/Front";
-    private boolean isConnected = false;
+    private String leftDistanceTopic = "/Group/16/LeftDistance";
+    private String middleDistanceTopic = "/Group/16/MiddleDistance";
+    private String rightDistanceTopic = "/Group/16/RightDistance";
+
+    private static final int IMAGE_WIDTH = 320; // **
+    private static final int IMAGE_HEIGHT = 240; // **
+
+    private String backMessage;
+    private String frontMessage;
+    private String leftMessage;
+    private String rightMessage;
+
+    private int progress = 0;
+
     MqttAndroidClient client;
+    private MqttClient mMqttClient;
 
-    int IMAGE_WIDTH = 411;
-    int IMAGE_HEIGHT = 250;
     final Bitmap bm = Bitmap.createBitmap(IMAGE_WIDTH, IMAGE_HEIGHT, Bitmap.Config.ARGB_8888);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         client = new MqttAndroidClient(MainActivity.this, MQTT_SERVER, TAG);
 
         MqttConnectOptions options = new MqttConnectOptions();
-        options.setUserName(USERNAME);//set the username
-        options.setPassword(PASSWORD.toCharArray());//set the username
+        options.setUserName(USERNAME); //set the username
+        options.setPassword(PASSWORD.toCharArray()); //set the username
 
         try {
-            client.connect(options,new IMqttActionListener() {
+            client.connect(options, new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    Log.e(TAG,"success");
+                    Log.e(TAG, "success");
                 }
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Log.e(TAG,"Fail");
+                    Log.e(TAG, "Fail");
                 }
             });
         } catch (MqttException e) {
@@ -85,35 +100,131 @@ public class MainActivity extends AppCompatActivity {
         Button Left = findViewById(R.id.buttonLeft);
         Button Right = findViewById(R.id.buttonRight);
         Button Stop = findViewById(R.id.buttonStop);
-        Switch Cruise = (Switch)findViewById(R.id.switchCruise);
+        Switch Cruise = (Switch) findViewById(R.id.switchCruise);
+        Cruise.setChecked(false);
         ImageView Stream = findViewById(R.id.imageView);
 
         ProgressBar LeftBar = findViewById(R.id.progressBarLeft);
         ProgressBar MiddleBar = findViewById(R.id.progressBarMiddle);
         ProgressBar RightBar = findViewById(R.id.progressBarRight);
 
-        //Timer for progressbar
 
-        new Timer().scheduleAtFixedRate(new TimerTask() {
+        Cruise.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void run() {
-                try {
-                    progressBar(leftDistanceTopic, LeftBar);
-                } catch (MqttException e) {
-                    e.printStackTrace();
+            public void onCheckedChanged(CompoundButton Cruise, boolean isChecked) {
+                String cruiseMessage = "Cruise";
+                //Prevent the listener from triggering during initialization
+                if (Cruise.isPressed() && isChecked)     // **
+                {
+                    try {
+                        client.publish(controlTopic, cruiseMessage.getBytes(), 0, false);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    //return to use manual control;
+                    String message = "Stop";
+                    try {
+                        client.publish(controlTopic, message.getBytes(), 0, false);
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
                 }
+            }
+        });
+
+        Stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //When "Stop" is clicked the car stop
+                String message = "Stop";
                 try {
-                    progressBar(rightDistanceTopic, RightBar);
-                } catch (MqttException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    progressBar(frontDistanceTopic, MiddleBar);
+                    client.publish(controlTopic, message.getBytes(), 0, false);
                 } catch (MqttException e) {
                     e.printStackTrace();
                 }
             }
-        }, 0, 35);
+        });
+
+        Ahead.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                String message = "";
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // button pressed
+                    message = "Ahead";
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // button released.
+                    message = "Stop";
+                }
+                try {
+                    client.publish(controlTopic, message.getBytes(), 0, false);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+        Back.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                String message = "";
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // button pressed
+                    message = "Back";
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // button released.
+                    message = "Stop";
+                }
+                try {
+                    client.publish(controlTopic, message.getBytes(), 0, false);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+        Left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                String message = "";
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // button pressed
+                    message = "Left";
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // button released.
+                    message = "Stop";
+                }
+                try {
+                    client.publish(controlTopic, message.getBytes(), 0, false);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+
+        Right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                String message = "";
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // button pressed
+                    message = "Right";
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // button released.
+                    message = "Stop";
+                }
+                try {
+                    client.publish(controlTopic, message.getBytes(), 0, false);
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
 
         Stream.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -126,163 +237,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        Cruise.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
-            @Override
-            public void onCheckedChanged(CompoundButton Cruise, boolean isChecked) {
-                String cruiseMessage = "Cruise";
-                //Prevent the listener from triggering during initialization
-                if (Cruise.isPressed()) {
-                    try {
-                        client.publish(controlTopic, cruiseMessage.getBytes(), 1, false);
-                    } catch (MqttException e) {
-                        e.printStackTrace();
-                    }
-                    return;
-                } else {
-                    //return to use manual control;
-                    String message = "Stop";
-                    try {
-                        client.publish(controlTopic, message.getBytes(), 1, false);
-                    } catch (MqttException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-
-        Stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //When "Stop" is clicked the car stop
-                String message = "Stop";
-
-                try {
-                    client.publish(controlTopic, message.getBytes(), 1, false);
-                } catch (MqttException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-
-        Ahead.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                String message =null;
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    message = "Forward";
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    message = "Stop";
-                }
-
-
-                try {
-                    client.publish(controlTopic, message.getBytes(), 1, false);
-                } catch (MqttException e) {
-                    e.printStackTrace();
-                }
-                return false;
-            }
-        });
-
-        Back.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                String message = null;
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    message = "Backward";
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    message = "Stop";
-                }
-
-
-                try {
-                    client.publish(controlTopic, message.getBytes(), 1, false);
-                } catch (MqttException e) {
-                    e.printStackTrace();
-                }
-                return false;
-            }
-        });
-
-        Left.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                String message = null;
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    message = "Left";
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    message = "Stop";
-                }
-
-
-                try {
-                    client.publish(controlTopic, message.getBytes(), 1, false);
-                } catch (MqttException e) {
-                    e.printStackTrace();
-                }
-                return false;
-            }
-        });
-
-        Right.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                String message = null;
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    message = "Right";
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    message = "Stop";
-                }
-
-
-                try {
-                    client.publish(controlTopic, message.getBytes(), 1, false);
-                } catch (MqttException e) {
-                    e.printStackTrace();
-                }
-
-                return false;
-            }
-        });
-
-
     }
 
+    public void progressBar(String topic, ProgressBar progressBar) throws MqttException {
 
-    public void progressBar (String topic, ProgressBar progressBar) throws MqttException {
+        IMqttToken subToken = client.subscribe(topic, 1);
+        subToken.setActionCallback(new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
 
+            }
 
-       client.setCallback(new MqttCallback() {
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+
+            }
+        });
+        client.setCallback(new MqttCallback() {
             @Override
             public void connectionLost(Throwable cause) {
-                isConnected = false;
-
-                final String connectionLost = "Connection to MQTT broker lost";
-                Log.w(TAG, connectionLost);
-                Toast.makeText(getApplicationContext(), connectionLost, Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
-                int progress = (Integer.parseInt(message.toString())/320)*100;
-                progressBar.setProgress(progress);
 
+                if (topic.equals(leftDistanceTopic)) {
+                    progress = (Integer.parseInt(message.toString()) / 320) * 100;
+
+                    progressBar.setProgress(progress);
+                }
             }
-
 
             @Override
             public void deliveryComplete(IMqttDeliveryToken token) {
-                Log.d(TAG, "Message delivered");
+
             }
         });
     }
+
 
     public void cameraView(String topic, ImageView camera) throws MqttException {
 
@@ -299,7 +291,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /* client.setCallback(new MqttCallback() {
+        client.setCallback(new MqttCallback() {
             @Override
             public void connectionLost(Throwable cause) {
             }
@@ -307,7 +299,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
                 if (topic.equals(cameraTopic)) {
-
+//                    final Bitmap bm = Bitmap.createBitmap(IMAGE_WIDTH, IMAGE_HEIGHT, Bitmap.Config.ARGB_8888);
                     final byte[] payload = message.getPayload();
                     final int[] colors = new int[IMAGE_WIDTH * IMAGE_HEIGHT];
                     for (int ci = 0; ci < colors.length; ++ci) {
@@ -318,18 +310,17 @@ public class MainActivity extends AppCompatActivity {
                     }
                     bm.setPixels(colors, 0, IMAGE_WIDTH, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
                     camera.setImageBitmap(bm);
-
+                } else {
                     Log.e(topic, "[MQTT] Topic: " + topic + " | Message: " + message.toString());
-
                 }
             }
 
             @Override
             public void deliveryComplete(IMqttDeliveryToken token) {
-
+                Log.d(TAG, "Message delivered");
             }
         });
-        */
     }
 
 }
+
